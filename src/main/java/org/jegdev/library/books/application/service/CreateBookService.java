8b6e986438fc9c64cab14c569c3f2e7e24da7ba4
JEGonDev <juan.dev.gonzalez@gmail.com>
2025-08-01 17:44:20 -0500

    Se crea el servicio para la creacion de un libro

--- src/main/java/org/jegdev/library/books/application/service/CreateBookService.java
+++ src/main/java/org/jegdev/library/books/application/service/CreateBookService.java
@@ -7,7 +7,14 @@ import org.jegdev.library.books.domain.model.Book;
 import org.jegdev.library.books.domain.port.in.CreateBookUseCase;
 import org.jegdev.library.books.domain.port.out.BookRepository;
 import org.jegdev.library.books.dto.BookRequest;
+import org.jegdev.library.books.dto.BookResponse;
+import org.jegdev.library.books.infrastructure.adapter.in.rest.mapper.BookDtoMapper;
 
+import java.time.Instant;
+
+/**
+ * Servicio que implementa el caso de uso para crear un libro.
+ */
 @ApplicationScoped // Crea una unica instancia de esta clase durante todo el ciclo de vida de la app
 public class CreateBookService implements CreateBookUseCase {
 
@@ -18,7 +25,13 @@ public class CreateBookService implements CreateBookUseCase {
     BookDtoMapper bookDtoMapper;
 
     @Override
-    public Uni<Book> create(BookRequest bookRequest) {
-        return null;
+    public Uni<BookResponse> create(BookRequest bookRequest) {
+        // Convertimos el DTO recibido en un objeto del modelo de dominio
+        Book book = bookDtoMapper.toDomain(bookRequest);
+        book.setCreatedAt(Instant.now()); // Asignamos la fecha de creación
+
+        // Guardamos el libro en el repositorio y transformamos el resultado en una respuesta
+        return bookRepository.save(book)
+                .map(bookDtoMapper::toResponse); // Convertimos el Book del dominio a BookResponse para el cliente
     }
 }
--- src/main/java/org/jegdev/library/books/domain/port/in/CreateBookUseCase.java
+++ src/main/java/org/jegdev/library/books/domain/port/in/CreateBookUseCase.java
@@ -1,13 +1,13 @@
 package org.jegdev.library.books.domain.port.in;
 
 import io.smallrye.mutiny.Uni;
-import org.jegdev.library.books.domain.model.Book;
 import org.jegdev.library.books.dto.BookRequest;
+import org.jegdev.library.books.dto.BookResponse;
 
 /**
  * Puerto de entrada para crear un libro.
  * Define la operación que puede invocar un adaptador de entrada (REST, eventos, etc).
  */
 public interface CreateBookUseCase {
-    Uni<Book> create(BookRequest bookRequest);
+    Uni<BookResponse> create(BookRequest bookRequest);
 }
